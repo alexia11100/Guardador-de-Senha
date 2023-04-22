6025d63e0b832f5843a9d4dce26215c0de7b41fa
Alexia Sacerdote 3 <127212707+alexia11100@users.noreply.github.com>
2023-04-22 03:14:37 -0300

    Novas funções
    .gitignore para ignorar arquivos irrelevantes e algoritmo simples para esconder as credenciais.

--- funcoes.py
+++ funcoes.py
@@ -1,18 +1,26 @@
 import os
+import base64
 
 # Função pra limpar a tela do terminal.
 def limpar_tela():
     os.system("cls")
 
+def criptografar(texto):
+    return base64.b64encode(texto.encode()).decode()
+
+def descriptografar(texto):
+    return base64.b64decode(texto).decode()
+
 # Função pra apagar as senhas do arquivo Senhas.txt.
 def limpar_senhas():
-    arquivo = open("Senhas.txt", "w")
+    arquivo = open("Senhas.txt", "w", encoding="utf-8")
     arquivo.write("")
+    arquivo.close()
     print("Arquivo de senha vazio.")
 
 # Função pra adicionar novas senhas (email, app e nova senha).
 def salvar_senha():
-    app = input("Que app vc deseja cadrastar a senha? ")
+    app = input("Que app vc deseja cadrastar a senha? ").title()
     email_do_app = input("Qual o email dessa conta? ")
     nova_senha = input("Informe a senha: ")
     limpar_tela()
@@ -24,14 +32,22 @@ def salvar_senha():
     limpar_tela()
 
     if confirmacao == "S":
-        arquivo = open("Senhas.txt", "a")
-        arquivo.write(f"{app}|{email_do_app}|{nova_senha}\n")
-        
+        try:
+            arquivo = open("Senhas.txt", "r", encoding="utf-8")
+        except FileNotFoundError:
+            limpar_senhas()
+            arquivo = open("Senhas.txt", "r", encoding="utf-8")
+        conteudo = descriptografar(arquivo.read())+f"{app}|{email_do_app}|{nova_senha}\n"
+        arquivo.close()
+
+        arquivo = open("Senhas.txt", "w", encoding="utf-8")
+        arquivo.write(criptografar(conteudo))
+        arquivo.close()
+
         print("Senha adicionada com sucesso!\n")
         registrar_nova = input("Digite \"S\" para registrar outra senha ou \"N\" para voltar ao menu: ")
-        
+        limpar_tela()
         if registrar_nova == "S":
-            limpar_tela()
             return salvar_senha()
         
     else:
@@ -39,6 +55,41 @@ def salvar_senha():
 
 # Função pra mostrar todas as senhas ja registradas no app.
 def mostrar_senhas():
-    arquivo = open("Senhas.txt", "r")
-    conteudo = arquivo.read()
-    print(conteudo)
+    arquivo = open("Senhas.txt", "r", encoding="utf-8")
+    conteudo = descriptografar(arquivo.read())
+    arquivo.close()
+    conteudo = conteudo.split("\n")[:-1]
+    
+    print("Credenciais cadastradas:\n")
+
+    for senha in conteudo:
+        credenciais = senha.split("|")
+    
+        print(f"Aplicativo: {credenciais[0]}")
+        print(f"Email: {credenciais[1]}")
+        print(f"Senha: {credenciais[2]}\n")
+
+def mostrar_senha_app():
+    arquivo = open("Senhas.txt", "r", encoding="utf-8")
+    conteudo = descriptografar(arquivo.read())
+    arquivo.close()
+    conteudo = conteudo.split("\n")[:-1]
+    
+    credenciais = []
+    apps = set()
+    for senha in conteudo:
+        credencial = senha.split("|")
+        credenciais.append(credencial)
+        apps.add(credencial[0])
+    apps = list(apps)
+    
+    for posicao in range(1,len(apps)+1):
+        print(posicao,"-",apps[posicao-1])
+
+    opcao = int(input("Escolha o app que deseja ver as senhas: "))
+    limpar_tela()
+    print("Credenciais cadastradas para o app "+credencial[0]+":\n")
+    for credencial in credenciais:
+        if credencial[0] == apps[opcao-1]:
+            print("Email: ",credencial[1])
+            print("Senha: ",credencial[2]+"\n")
--- programa.py
+++ programa.py
@@ -8,19 +8,20 @@ while True:
     print("Seja bem-vindo ao Guardador de Senhas!")
     print("1- Guardar nova senha")
     print("2- Mostrar todas as senhas")
-    print("3- Apagar senhas")
+    print("3- Mostrar senha por app")
+    print("4- Apagar senhas")
 
     opcao = int(input("Escolha a opção: "))
+    
     limpar_tela()
     if opcao == 1:
         salvar_senha()
-        input("Aperte \"ENTER\" para voltar ao menu.")
     elif opcao == 2:
         mostrar_senhas()
-        input("Aperte \"ENTER\" para voltar ao menu.")
     elif opcao == 3:
+        mostrar_senha_app()
+    elif opcao == 4:
         limpar_senhas()
-        input("Aperte \"ENTER\" para voltar ao menu.")
     else:
         print("Opção inválida")
-        input("Aperte \"ENTER\" para voltar ao menu.")
+    input("Aperte \"ENTER\" para voltar ao menu.")
